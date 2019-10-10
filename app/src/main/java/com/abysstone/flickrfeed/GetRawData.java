45df8cb88ca571a4e8251cb2a222ef7fde7a2d75
abbystone <manasbhatt007@gmail.com>
2019-10-10 14:24:48 +0530

    Json in

---
+++ app/src/main/java/com/abysstone/flickrfeed/GetRawData.java
@@ -0,0 +1,95 @@
+package com.abysstone.flickrfeed;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+enum DownloadStatus { IDLE, PROCESSING, NOT_INITIALIZED, FAILED_OR_EMPTY, OK };
+
+class GetRawData extends AsyncTask<String, Void, String> {
+    private static final String TAG ="GetRawData";
+
+    private DownloadStatus mDownloadStatus;
+    private final onDownloadComplete mCallBack;
+
+    interface onDownloadComplete{
+//        void onDownloadComplete(String data, DownloadStatus status);
+
+        void OnDownloadComplete(String s, DownloadStatus mDownloadStatus);
+    }
+
+    public GetRawData(onDownloadComplete callback){
+        this.mDownloadStatus = DownloadStatus.IDLE;
+        mCallBack = callback;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        Log.d(TAG, "onPostExecute: parameter "+ s);
+//        super.onPostExecute(s);
+        if(mCallBack != null){
+            mCallBack.OnDownloadComplete(s,mDownloadStatus);
+        }
+        Log.d(TAG, "onPostExecute: ends!");
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+
+        if(strings == null){
+            mDownloadStatus = DownloadStatus.NOT_INITIALIZED;
+            return null;
+        }
+
+        try {
+            mDownloadStatus = DownloadStatus.PROCESSING;
+            URL url = new URL(strings[0]);
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            int response = connection.getResponseCode();
+            Log.d(TAG, "doInBackground: The response code was "+ response);
+
+            StringBuilder result = new StringBuilder();
+
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String line;
+            while (null != (line = reader.readLine())){
+                result.append(line).append("\n");
+            }
+
+            mDownloadStatus = DownloadStatus.OK;
+            return result.toString();
+
+        }catch (MalformedURLException e){
+            Log.e(TAG, "doInBackground: Invalid Url" + e.getMessage());
+        }catch (IOException e){
+            Log.e(TAG, "doInBackground: IO exception reading data " + e.getMessage());
+        }catch (SecurityException e){
+            Log.e(TAG, "doInBackground: Security Exception.. Needs permission? " + e.getMessage());
+        }finally {
+            if (connection != null){
+                connection.disconnect();
+            }
+            if (reader != null){
+                try {
+                    reader.close();
+                }catch (IOException e){
+                    Log.e(TAG, "doInBackground: Error closing stream" + e.getMessage());
+                }
+            }
+        }
+        mDownloadStatus = DownloadStatus.FAILED_OR_EMPTY;
+        return null;
+    }
+}
---
+++ app/src/main/java/com/abysstone/flickrfeed/MainActivity.java
@@ -0,0 +1,68 @@
+package com.abysstone.flickrfeed;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity implements GetRawData.onDownloadComplete{
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        GetRawData getRawData = new GetRawData(this);
+        getRawData.execute("https://www.flickr.com/services/feeds/photos_public.gne?tags=android,nougat,sdk&tagmode=any&format=json&nojsoncallback=1");
+
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void OnDownloadComplete(String data, DownloadStatus status){
+        if (status == DownloadStatus.OK){
+            Log.d(TAG, "OnDownloadComplete: Data is "+ data);
+        }else {
+            //download or processing failed...
+            Log.d(TAG, "OnDownloadComplete: failed with status "+ status);
+        }
+    }
+
+
+}
