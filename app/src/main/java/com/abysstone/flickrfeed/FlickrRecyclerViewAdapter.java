472807be165f6bdedd420a681584602ae5545b1e
abbystone <manasbhatt007@gmail.com>
2019-10-13 04:33:23 +0530

    pulling up RecyclerView and Picasso

---
+++ app/src/main/java/com/abysstone/flickrfeed/FlickrRecyclerViewAdapter.java
@@ -0,0 +1,81 @@
+package com.abysstone.flickrfeed;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+class FlickrRecyclerViewAdapter extends RecyclerView.Adapter<FlickrRecyclerViewAdapter.FlickrImageViewHolder> {
+
+    private static final String TAG = "FlickrRecyclerViewAdapt";
+    private List<Photo> mPhotosList;
+    private Context context;
+
+    public FlickrRecyclerViewAdapter(Context context, List<Photo> mPhotosList) {
+        this.context = context;
+        this.mPhotosList = mPhotosList;
+    }
+
+    @NonNull
+    @Override
+    public FlickrImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //called by the layout manager when it needs a new view
+        Log.d(TAG, "onCreateViewHolder: new view requested");
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.browse, parent, false);
+        return new FlickrImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FlickrImageViewHolder holder, int position) {
+        //called by layout manager when it wants new data in an existing row.
+
+        Photo photoItem = mPhotosList.get(position);
+        Log.d(TAG, "onBindViewHolder: " + photoItem.getmTitle() + " --> " + position);
+        Picasso.with(context).load(photoItem.getmImage())
+                .error(R.drawable.placeholder)
+                .placeholder(R.drawable.placeholder)
+                .into(holder.thumbnail);
+
+        holder.title.setText(photoItem.getmTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.d(TAG, "getItemCount: called");
+        return ((mPhotosList != null) && (mPhotosList.size() != 0) ? mPhotosList.size() : 0);
+    }
+
+    void loadNewData(List<Photo> newPhotos){
+        mPhotosList = newPhotos;
+        notifyDataSetChanged();
+    }
+
+    public Photo getPhoto(int position){
+        return ((mPhotosList != null) && (mPhotosList.size() != 0) ? mPhotosList.get(position) : null);
+    }
+
+    static class FlickrImageViewHolder extends RecyclerView.ViewHolder{
+        private static final String TAG = "FlickrImageViewHolder";
+        ImageView thumbnail = null;
+        TextView title = null;
+
+        public FlickrImageViewHolder(View itemView){
+            super(itemView);
+            Log.d(TAG, "FlickrImageViewHolder: starts");
+            this.thumbnail = (ImageView) itemView.findViewById(R.id.thumbnail);
+            this.title = (TextView) itemView.findViewById(R.id.title);
+
+        }
+    }
+
+}
